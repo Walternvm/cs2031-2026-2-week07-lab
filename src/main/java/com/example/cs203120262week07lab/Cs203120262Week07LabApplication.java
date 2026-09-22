bd3fc18b38f087df167bfd531d9e4f92bdffb916
walteraquino <walter.aquino@utec.edu.pe>
2026-09-22 09:17:56 -0500

    Firts Commit

---
+++ src/main/java/com/example/cs203120262week07lab/Cs203120262Week07LabApplication.java
@@ -0,0 +1,13 @@
+package com.example.cs203120262week07lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs203120262Week07LabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cs203120262Week07LabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cs203120262week07lab/Cs203120262Week07LabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cs203120262week07lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs203120262Week07LabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
